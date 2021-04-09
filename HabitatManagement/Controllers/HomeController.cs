cb7124e9337ee9d9db5bf5a6890b0106545583d5
Vaibhav Vaish <vaibhav.vaish25@gmail.com>
2021-04-09 18:46:21 +0530

    First Form Created

--- HabitatManagement/Controllers/HomeController.cs
+++ HabitatManagement/Controllers/HomeController.cs
@@ -35,8 +35,7 @@ namespace HabitatManagement.Controllers
         public JsonResult GetFormDesignerColumnNames()
         {
             try
-            {
-                // Report bundles jqgrid column names
+            {               
                 string[] columnNames =
                 {
                     "Id",
@@ -108,5 +107,93 @@ namespace HabitatManagement.Controllers
                 return Json(jsonData);
             }
         }
+
+        public ActionResult PermitFormScreenDesignTemplate(int formID)
+        {
+            PermitFormScreenDesignTemplateBE model = new PermitFormScreenDesignTemplateBE();
+
+            if (formID <= 0)
+            {
+                model.Active = true;
+            }
+            else
+            {
+                PermitFormScreenDesignTemplateBE conditionFeedbackTemplate = FormLogic.FetchPermitFormScreenDesignTemplate(formID);
+                if (conditionFeedbackTemplate != null)
+                {
+                    model.FormID = conditionFeedbackTemplate.FormID;
+                    model.Design = conditionFeedbackTemplate.Design;
+                    model.Description = conditionFeedbackTemplate.Description;
+                    model.Active = conditionFeedbackTemplate.Active;
+                }
+            }
+            return View("PermitForm", model);
+        }
+
+        [HttpPost]
+        public ActionResult PermitFormScreenDesignTemplate(PermitFormScreenDesignTemplateBE model)
+        {
+
+            bool success = false;
+            int id = 0;
+
+            PermitFormScreenDesignTemplateBE permitFormScreenDesignTemplate = FormLogic.FetchPermitFormScreenDesignTemplate(model.FormID);
+            if (permitFormScreenDesignTemplate == null)
+            {
+                permitFormScreenDesignTemplate = new PermitFormScreenDesignTemplateBE();
+            }
+            permitFormScreenDesignTemplate.Design = model.Design.ToUpper();
+            permitFormScreenDesignTemplate.Description = model.Description;
+            permitFormScreenDesignTemplate.Active = model.Active;
+
+            if (model.FormID <= 0)
+            {
+                permitFormScreenDesignTemplate.CreatedBy = "Habitat";
+                permitFormScreenDesignTemplate.CreatedDateTime = DateTime.Now;
+                permitFormScreenDesignTemplate.LastUpdatedDateTime = DateTime.Now;
+                success = FormLogic.AddPermitFormScreenDesignTemplate(permitFormScreenDesignTemplate, out id);
+            }
+            else
+            {
+                permitFormScreenDesignTemplate.UpdatedBy = "Habitat";
+                permitFormScreenDesignTemplate.LastUpdatedDateTime = DateTime.Now;
+                success = FormLogic.UpdatePermitFormScreenDesignTemplate(permitFormScreenDesignTemplate);
+            }
+            return Json(new { success, id });
+        }
+
+        public ActionResult PermitFormScreenDesignTemplateDetail(int formID)
+        {
+            PermitFormScreenDesignTemplateDetailModelBE model = new PermitFormScreenDesignTemplateDetailModelBE();
+           
+            model.TemplateDetails = new List<PermitFormScreenDesignTemplateDetailBE>();
+
+            PermitFormScreenDesignTemplateBE permitFormScreenDesignTemplate = FormLogic.FetchPermitFormScreenDesignTemplate(formID);
+            if (permitFormScreenDesignTemplate != null)
+            {
+                model.FormID = permitFormScreenDesignTemplate.FormID;
+                model.Design = permitFormScreenDesignTemplate.Design;
+                model.Description = permitFormScreenDesignTemplate.Description;
+                model.Active = permitFormScreenDesignTemplate.Active;
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult PermitFormScreenDesignTemplateDetail(PermitFormScreenDesignTemplateDetailModelBE model)
+        {
+            bool success = false;
+            //success = FormLogic.Save(model.TemplateDetails);
+            return new JsonResult(new { Success = success, TemplateDetails = success ? model.TemplateDetails : null });
+        }
+
+
+        public ActionResult PermitFormTemplateFields(int formID)
+        {
+            List<PermitFormScreenDesignTemplateDetailBE> templateDetails = FormLogic.FetchAllPermitFormScreenDesignTemplateDetail(formID);            
+            
+            return View();
+        }
+
     }
 }
---
+++ HabitatManagement/Models/TaskFeedbackTemplateDetailModelBE.cs
@@ -0,0 +1,11 @@
+﻿
+using HabitatManagement.BusinessEntities;
+using System.Collections.Generic;
+
+namespace HabitatManagement.Models
+{
+    public class PermitFormScreenDesignTemplateDetailModelBE : PermitFormScreenDesignTemplateBE
+    {
+        public List<PermitFormScreenDesignTemplateDetailBE> TemplateDetails { get; set; }
+    }
+}
