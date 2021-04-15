afd78e4317dce38b84d59e454e16f6b93b670433
rahul verma <rahul.verma@RSK-BSL.COM>
2021-04-15 10:09:19 +0530

    Changes for form field type

--- HabitatManagement.BusinessEntities/Fields/FormFieldType.cs
+++ HabitatManagement.BusinessEntities/Fields/FormFieldType.cs
@@ -9,17 +9,17 @@ namespace HabitatManagement.BusinessEntities
     {
         //None = 0,
         Textbox = 1,
-        Checkbox = 2,
-        Radio = 3,
-        Dropdown = 4,
+       // Checkbox = 2,
+       // Radio = 3,
+       // Dropdown = 4,
         Date = 5,
         DateAndTime = 6,
         Signature = 7,
         TextArea = 8,
         CheckList = 9,
-        Table = 10,
-        Email = 11,
-        Image = 12,
+       // Table = 10,
+       // Email = 11,
+       // Image = 12,
         Label = 13,
     }
 }
