51c0cd1feb5704cbf0e19a0dea4a604ccc862ac2
rahul verma <rahul.verma@RSK-BSL.COM>
2021-04-19 17:58:57 +0530

    Changed for TableFieldTypeMaster

---
+++ HabitatManagement.BusinessEntities/Entities/TableFieldTypeMasterBE.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabitatManagement.BusinessEntities
+{
+    public class TableFieldTypeMasterBE
+    {
+        public int Id { get; set; }
+        public int Field { get; set; }
+        public string ColumnName { get; set; }
+        public int RowCount { get; set; }
+        public int ColumnType { get; set; }
+    }
+}
